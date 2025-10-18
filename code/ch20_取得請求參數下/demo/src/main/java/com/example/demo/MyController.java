7789629fe218bc5564d1cc382687c6f9355a6550
blueskyer1998 <litpm1998@gmail.com>
2025-10-18 16:51:09 +0800

    取得請求參數(下) - @RequestHeader、@PathVariable

---
+++ code/ch20_取得請求參數下/demo/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+public class MyController {
+    
+    @RequestMapping("/test")
+    public Student test() {
+        Student student = new Student();
+        student.setId(123);
+        student.setName("Eason");
+        return student;
+    }
+    
+    @RequestMapping("/product")
+    public Store product() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("蘋果");
+        list.add("橘子");
+        store.setProductList(list);
+        return store;
+    }
+    
+    @RequestMapping("/test1")
+    public String test1(@RequestParam Integer id, String name) {
+        System.out.println("id 的值為: " + id);
+        System.out.println("name 的值為: " + name);
+        return "請求成功";
+    }
+    
+    @RequestMapping("/test2")
+    public String test2(@RequestBody Student student) {
+        System.out.println("student 中的 id 值為: " + student.getId());
+        System.out.println("student 中的 name 值為:" + student.getName());
+        return "請求成功";
+    }
+    
+    @RequestMapping("/test3")
+    public String test3(@RequestHeader String info) {
+        System.out.println("info 的值為: " + info);
+        return "請求成功";
+    }
+    
+    @RequestMapping("/test4/{id}")
+    public String test4(@PathVariable Integer id) {
+        System.out.println("id 的值為: " + id);
+        return "請求成功";
+    }
+}

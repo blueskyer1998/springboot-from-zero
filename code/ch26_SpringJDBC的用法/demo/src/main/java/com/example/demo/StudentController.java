825d5305890b9749b709900ce1f95075c84da78e
blueskyer1998 <litpm1998@gmail.com>
2025-10-23 03:00:19 +0800

    SpringJDBC - 執行 INSERT、UPDATE、DELETE SQL

---
+++ code/ch26_SpringJDBC的用法/demo/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @RequestMapping("/students")
+    public String insert(@RequestBody Student student) {
+        String sql = "insert into student(id,name) values (:studentId, :studentName)";
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("studentId", student.getId());
+        map.put("studentName", student.getName());
+
+        namedParameterJdbcTemplate.update(sql,map);
+
+        return "執行 INSERT SQL";
+    }
+
+}
